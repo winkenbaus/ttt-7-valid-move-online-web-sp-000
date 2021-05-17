4b4e8e16cfe209fb9780f0cb6e86a23e74bd3705
caitlin <caitlin.wink@gmail.com>
2021-05-17 16:50:12 -0700

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@
 def valid_move?(board, index)
     if position_taken?(board, index)
         false
-    elsif index >  -1 && index < 10
+    elsif index >  -1 && index < 9
         true
     else
         false 
